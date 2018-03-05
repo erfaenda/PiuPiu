9ec2b0218cd2e6ef4b0c8d5c53fade2f15caa414
alex <regalko7@gmail.com>
2018-03-05 08:52:19 +0500

    erf

--- Base/OOP/MetaClass.py
+++ Base/OOP/MetaClass.py
@@ -1,10 +1,13 @@
-class SchoolMember:
+from abc import *
+
+class SchoolMember(metaclass=ABCMeta):
     '''Представляет любого человека в школе.'''
     def __init__(self, name, age):
         self.name = name
         self.age = age
         print('(Создан SchoolMember: {0})'.format(self.name))
 
+    @abstractmethod
     def tell(self):
         '''Вывести информацию.'''
         print('Имя:"{0}" Возраст:"{1}"'.format(self.name, self.age), end=" ")
@@ -18,6 +21,7 @@ class Teacher(SchoolMember):
     def tell(self):
         SchoolMember.tell(self)
         print('Зарплата: "{0:d}"'.format(self.salary))
+
 class Student(SchoolMember):
     '''Представляет студента.'''
     def __init__(self, name, age, marks):
@@ -30,7 +34,12 @@ class Student(SchoolMember):
 
 t = Teacher('Mrs. Shrividya', 40, 30000)
 s = Student('Swaroop', 25, 75)
+#m = SchoolMember('abc', 10)
+# Это приведёт к ошибке: "TypeError: Can't instantiate abstract class
+# SchoolMember with abstract methods tell"
+
 print() # печатает пустую строку
 members = [t, s]
+
 for member in members:
     member.tell() # работает как для преподавателя, так и для студента
