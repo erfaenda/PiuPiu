88d5d4a8adc3126b1b7fc457b827067dd6ef061c
erfaenda <regalko7@gmail.com>
2018-03-17 13:36:18 +0500

    йцуйцу

--- PyQTT/Fist_self_text_program/self_test_program_xml.py
+++ PyQTT/Fist_self_text_program/self_test_program_xml.py
@@ -1,44 +1,61 @@
 import sys
 import random
+import xml.dom.minidom
 # Импортируем наш интерфейс из файла
 from PyQTT.Fist_self_text_program.self_test_programGui import *
 from PyQt5 import QtCore, QtGui, QtWidgets
 
 class MyWin(QtWidgets.QMainWindow):
 
-    correct = "2 + 2 = 4"
-    correct2 = "Плоская Земля! Ну вы че епта! Ну плоская же она!"
-    correct3 = "Вернее не только лишь все, мало кто может это делать"
-
-    variants1 = [correct, 'На траве дрова', 'Путин 2018']
-    variants2 = ['Шар', 'Квадрат', correct2]
-    variants3 = [correct3, 'Кличко ты ли это','Украина це Европа']
+    text = []
+    questions = []
+    variants = []
+    correct = []
 
     def __init__(self, parent=None):
         QtWidgets.QWidget.__init__(self, parent)
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
-        # блокирую вкладки таблицы
-        self.ui.Tab3.setTabEnabled(1, False)
-        self.ui.Tab3.setTabEnabled(2, False)
+
+        self.dom = xml.dom.minidom.parse('base.xml')
+        self.collection = self.dom.documentElement
+        self.linesArr = self.collection.getElementsByTagName("text")
+
+        for line in self.linesArr:
+            self.text.append(line.childNodes[0].data)
+            self.questions.append(line.getAttribute('question'))
+            self.variants.append(line.getAttribute('answers').split('**?**'))
+            self.correct.append(line.getAttribute('correct'))
 
         # Перетасовка вариантов
-        random.shuffle(self.variants1)
-        random.shuffle(self.variants2)
-        random.shuffle(self.variants3)
+        random.shuffle(self.variants[0])
+        random.shuffle(self.variants[1])
+        random.shuffle(self.variants[2])
+
+        # Возможно это позиционирование текста который надо изучить
+        self.ui.textEdit.setText(self.text[0])
+        self.ui.textEdit_2.setText(self.text[1])
+        self.ui.textEdit_3.setText(self.text[2])
+        # Позиционирование вопроса
+        self.ui.label.setText(self.questions[0])
+        self.ui.label_2.setText(self.questions[1])
+        self.ui.label_3.setText(self.questions[2])
 
         # Перетасовка кнопок
-        self.ui.radioButton.setText(self.variants1[0])
-        self.ui.radioButton_2.setText(self.variants1[1])
-        self.ui.radioButton_3.setText(self.variants1[2])
+        self.ui.radioButton.setText(self.variants[0][0])
+        self.ui.radioButton_2.setText(self.variants[0][1])
+        self.ui.radioButton_3.setText(self.variants[0][2])
 
-        self.ui.radioButton_4.setText(self.variants2[0])
-        self.ui.radioButton_5.setText(self.variants2[1])
-        self.ui.radioButton_6.setText(self.variants2[2])
+        self.ui.radioButton_4.setText(self.variants[1][0])
+        self.ui.radioButton_5.setText(self.variants[1][1])
+        self.ui.radioButton_6.setText(self.variants[1][2])
 
-        self.ui.radioButton_7.setText(self.variants3[0])
-        self.ui.radioButton_8.setText(self.variants3[1])
-        self.ui.radioButton_9.setText(self.variants3[2])
+        self.ui.radioButton_7.setText(self.variants[2][0])
+        self.ui.radioButton_8.setText(self.variants[2][1])
+        self.ui.radioButton_9.setText(self.variants[2][2])
+
+        self.ui.Tab3.setTabEnabled(1, False)
+        self.ui.Tab3.setTabEnabled(2, False)
 
         # Здесь прописываем событие нажатия на кнопку
         self.ui.buttonGroup.buttonClicked.connect(self.correctAns1)
@@ -54,7 +71,7 @@ class MyWin(QtWidgets.QMainWindow):
     def correctAns1(self):
         for rb in self.ui.buttonGroup.buttons():
             if rb.isChecked():
-                if rb.text() == self.correct:
+                if rb.text() == self.correct[0]:
                     self.ui.statusbar.showMessage("Ответ верный, вторая вкладка таблицы разблокирована!", 5000)
                     self.ui.tab_1.setEnabled(False)
                     self.ui.Tab3.setTabEnabled(1, True)
@@ -62,7 +79,7 @@ class MyWin(QtWidgets.QMainWindow):
     def correctAns2(self):
         for rb in self.ui.buttonGroup_2.buttons():
             if rb.isChecked():
-                if rb.text() == self.correct2:
+                if rb.text() == self.correct[1]:
                     self.ui.statusbar.showMessage("Ответ верный, третья вкладка таблицы разблокирована!", 5000)
                     self.ui.tab_2.setEnabled(False)
                     self.ui.Tab3.setTabEnabled(2, True)
@@ -70,9 +87,9 @@ class MyWin(QtWidgets.QMainWindow):
     def correctAns3(self):
         for rb in self.ui.buttonGroup_3.buttons():
             if rb.isChecked():
-                if rb.text() == self.correct3:
+                if rb.text() == self.correct[2]:
                     self.ui.statusbar.showMessage("Ответ верный, вторая вкладка таблицы разблокирована!", 5000)
-                    self.ui.Tab3.setTabEnabled(False)
+                    self.ui.tab_3.setEnabled(False)
 
 
 
