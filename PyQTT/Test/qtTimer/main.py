26df2ff0e414fb1fc5ec140dad7941e4a1a9f3bf
alex <regalko7@gmail.com>
2018-04-24 15:03:27 +0500

    erfsdfdsfа

--- PyQTT/Test/qtTimer/main.py
+++ PyQTT/Test/qtTimer/main.py
@@ -4,6 +4,7 @@ from datetime import timedelta, datetime
 from PyQTT.Test.qtTimer.gui import *
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtCore import Qt, QTimer
+from PyQt5.QtWidgets import QStyleFactory
 
 class MyWin(QtWidgets.QMainWindow):
 
@@ -20,16 +21,17 @@ class MyWin(QtWidgets.QMainWindow):
     gigro = 0
 
     # Logic value
-    deviceLogic_state = True
+    deviceLogic_state = False
+    timeLogic_state = False
     light = []
     ballu = []
     vlaga = []
 
     def __init__(self, parent=None):
         QtWidgets.QWidget.__init__(self, parent)
-        self.ui = Ui_Form()
+        self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
-        #self.uptime()
+        self.uptime()
 
         # signals and slots
         self.ui.pushButton.clicked.connect(self.sw1_on)
@@ -41,7 +43,9 @@ class MyWin(QtWidgets.QMainWindow):
         self.ui.pushButton_7.clicked.connect(self.sw4_on)
         self.ui.pushButton_8.clicked.connect(self.sw4_off)
         self.ui.pushButton_logic_on.clicked.connect(self.device_logicON)
-        self.ui.pushButton_logic_on.clicked.connect(self.device_logicOFF)
+        self.ui.pushButton_logic_off.clicked.connect(self.device_logicOFF)
+        self.ui.pushButton_logicTimer_on.clicked.connect(self.time_logicON)
+        self.ui.pushButton_logicTimer_off.clicked.connect(self.time_logicOFF)
 
 
     # main function read devices value and control gpio ports
@@ -68,7 +72,7 @@ class MyWin(QtWidgets.QMainWindow):
     def dateTime(self):
         #now = datetime.now()
         now = datetime.strftime(datetime.now(), "%H:%M:%S")
-        self.ui.label_6.setText(str(now))
+        self.ui.label_6.setText('Системное время: ' + str(now))
 
     def uptime(self):
         with open('/proc/uptime', 'r') as f:
@@ -78,23 +82,43 @@ class MyWin(QtWidgets.QMainWindow):
         self.ui.label.setText(uptime_string)
 
     def check_state(self):
+        # check state buttons on/off
         if self.sw_state1 == False:
-            self.ui.label_2.setText('Порт № 1 - OFF')
-        else:self.ui.label_2.setText('Порт № 1 - ON')
+            self.ui.label_status_port1.setText('OFF')
+        else:self.ui.label_status_port1.setText('ON')
         if self.sw_state2 == False:
-            self.ui.label_3.setText('Порт № 2 - OFF')
-        else:self.ui.label_3.setText('Порт № 2 - ON')
+            self.ui.label_status_port2.setText('OFF')
+        else:self.ui.label_status_port2.setText('ON')
         if self.sw_state3 == False:
-            self.ui.label_4.setText('Порт № 3 - OFF')
-        else:self.ui.label_4.setText('Порт № 3 - ON')
+            self.ui.label_status_port3.setText('OFF')
+        else:self.ui.label_status_port3.setText('ON')
         if self.sw_state4 == False:
-            self.ui.label_5.setText('Порт № 4 - OFF')
-        else:self.ui.label_5.setText('Порт № 4 - ON')
+            self.ui.label_status_port4.setText('OFF')
+        else:self.ui.label_status_port4.setText('ON')
+        # check state logic on/off
+        if self.deviceLogic_state == False:
+            self.ui.label__status_logic.setText('Logic disable')
+        else:
+            self.ui.label__status_logic.setText('Logic enable')
+
+        if self.timeLogic_state == False:
+            self.ui.label_time_status.setText('Logic disable')
+        else:
+            self.ui.label_time_status.setText('Logic enable')
 
     def device_logicON(self):
         self.deviceLogic_state = True
+        print(self.deviceLogic_state)
+
     def device_logicOFF(self):
         self.deviceLogic_state = False
+        print(self.deviceLogic_state)
+
+    def time_logicON(self):
+        self.timeLogic_state = True
+
+    def time_logicOFF(self):
+        self.timeLogic_state = False
 
     # buttons ON/OFF
     def sw1_on(self):
@@ -139,12 +163,13 @@ class MyWin(QtWidgets.QMainWindow):
 
 if __name__=="__main__":
     app = QtWidgets.QApplication(sys.argv)
+    app.setStyle(QStyleFactory.create('Oxygen'))
     myapp = MyWin()
     timer = QTimer()
     timer.timeout.connect(myapp.check_state)
     timer.start(100)
     timeing = QTimer()
-    #timeing.timeout.connect(myapp.uptime)
+    timeing.timeout.connect(myapp.uptime)
     timeing.timeout.connect(myapp.dateTime)
     timeing.timeout.connect(myapp.switch_light)
     timeing.timeout.connect(myapp.readDevices)
