18cf89fa074532efb2b1384bc9af9c744208f648
alex <regalko7@gmail.com>
2018-04-25 19:52:27 +0500

    xx

---
+++ Base/Time/timee.py
@@ -0,0 +1,5 @@
+from datetime import *
+
+d = datetime.now()
+a = str(d.hour) + str(d.minute)
+print(a) # 10
--- PyQTT/Gpio_control/Gpio_control_main.py
+++ PyQTT/Gpio_control/Gpio_control_main.py
@@ -13,6 +13,11 @@ class MyWin(QtWidgets.QMainWindow):
     sw_state2 = False
     sw_state3 = False
     sw_state4 = False
+    sw_state5 = False
+    sw_state6 = False
+    sw_state7 = False
+    sw_state8 = False
+    list_state =[sw_state1, sw_state2]
 
     # devices value
     temp_1 = 0
@@ -38,8 +43,6 @@ class MyWin(QtWidgets.QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
         #self.uptime()
-        # ini
-
         # signals and slots
         self.ui.pushButton.clicked.connect(self.sw1_on)
         self.ui.pushButton_2.clicked.connect(self.sw1_off)
@@ -53,20 +56,48 @@ class MyWin(QtWidgets.QMainWindow):
         self.ui.pushButton_logic_off.clicked.connect(self.device_logicOFF)
         self.ui.pushButton_logicTimer_on.clicked.connect(self.time_logicON)
         self.ui.pushButton_logicTimer_off.clicked.connect(self.time_logicOFF)
+    # test function
+    def test(self):
+        t1 = self.ui.timeEdit.time()
+        now = datetime.now()
+        today = now.replace(hour=t1.hour(), minute=t1.minute(), second=0, microsecond=0)
 
-    # main fuction read time and control gpio ports
+        print(now > today)
+
+    # universal main fuction read time and control gpio ports
     def timer_logic(self):
         if self.timeLogic_state == True:
-            time_min = self.ui.timeEdit.text()
-            time_max = self.ui.timeEdit_2.text()
-            #time_min = time_min.replace(':', '')
-            self.dnat_min = str(time_min.hour()) + str(time_min.minute())
-            self.dnat_max = time_max.hour() + time_max.minute()
-            if self.globalTime < self.time_min or self.globalTime > self.dnat_max:
-                self.sw1_on()
-            else:
+            time_min = self.ui.timeEdit.time()
+            time_max = self.ui.timeEdit_2.time()
+            now = datetime.now()
+            self.dnat_min = now.replace(hour=time_min.hour(), minute=time_min.minute(), second=0, microsecond=0)
+            self.dnat_max = now.replace(hour=time_max.hour(), minute=time_max.minute(), second=0, microsecond=0)
+            # off range
+            if now >= self.dnat_min and now <= self.dnat_max:
                 self.sw1_off()
-            print(time_min)
+            else:
+                self.sw1_on()
+
+     # universal main fuction read time and control gpio ports
+    def uni_timer_logic(self, time_min, time_max, sw_state):
+        if self.timeLogic_state == True:
+            if self.chekbox_list[chk]
+            now = datetime.now()
+            self.dnat_min = now.replace(hour=time_min.hour(), minute=time_min.minute(), second=0,
+                                                    microsecond=0)
+            self.dnat_max = now.replace(hour=time_max.hour(), minute=time_max.minute(), second=0,
+                                                    microsecond=0)
+            # off range
+            if now >= self.dnat_min and now <= self.dnat_max:
+                self.list_state[sw_state] = False
+            else:
+                self.list_state[sw_state] = True
+
+    def starter_all_timer_logic(self):
+        self.uni_timer_logic(self.ui.timeEdit.time(), self.ui.timeEdit_2.time(), 0)
+        self.uni_timer_logic(self.ui.timeEdit_3.time(), self.ui.timeEdit_4.time(), 1)
+
+
 
     # main function read devices value and control gpio ports
     def deviceLogic(self):
@@ -88,14 +119,13 @@ class MyWin(QtWidgets.QMainWindow):
         self.ui.midtemp.setValue(self.middle_temp)
 
 
-
-
     def dateTime(self):
-        #now = datetime.now()
+        # display time
         now = datetime.strftime(datetime.now(), "%H:%M:%S")
-        self.globalTime = int(datetime.strftime(datetime.now(), "%H%M"))
         self.ui.label_6.setText('Системное время: ' + str(now))
-        print(self.globalTime)
+        time = datetime.now()
+        #print('time ' + str(self.globalTime))
+        print(self.list_state[0])
 
     def uptime(self):
         with open('/proc/uptime', 'r') as f:
@@ -104,20 +134,29 @@ class MyWin(QtWidgets.QMainWindow):
 
         self.ui.label.setText(uptime_string)
 
+        # тут надо переписать как то модно
+    '''def check_state_2(self):
+        for state in self.list_state:
+            if state == False:'''
+
     def check_state(self):
         # check state buttons on/off
-        if self.sw_state1 == False:
+        if self.list_state[0] == False:
             self.ui.label_status_port1.setText('OFF')
-        else:self.ui.label_status_port1.setText('ON')
-        if self.sw_state2 == False:
+        else:
+            self.ui.label_status_port1.setText('ON')
+        if self.list_state[1] == False:
             self.ui.label_status_port2.setText('OFF')
-        else:self.ui.label_status_port2.setText('ON')
+        else:
+            self.ui.label_status_port2.setText('ON')
         if self.sw_state3 == False:
             self.ui.label_status_port3.setText('OFF')
-        else:self.ui.label_status_port3.setText('ON')
+        else:
+            self.ui.label_status_port3.setText('ON')
         if self.sw_state4 == False:
             self.ui.label_status_port4.setText('OFF')
-        else:self.ui.label_status_port4.setText('ON')
+        else:
+            self.ui.label_status_port4.setText('ON')
         # check state logic on/off
         if self.deviceLogic_state == False:
             self.ui.label__status_logic.setText('Logic disable')
@@ -197,7 +236,8 @@ if __name__=="__main__":
     timeing.timeout.connect(myapp.switch_light)
     timeing.timeout.connect(myapp.readDevices)
     timeing.timeout.connect(myapp.deviceLogic)
-    timeing.timeout.connect(myapp.timer_logic)
+    #timeing.timeout.connect(myapp.timer_logic)
+    timeing.timeout.connect(myapp.starter_all_timer_logic)
     timeing.start(1000)
     myapp.show()
     sys.exit(app.exec_())
