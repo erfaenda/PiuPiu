89846d55675512d588189f5d1f868b62d61434dc
erfaenda <regalko7@gmail.com>
2019-02-11 23:10:37 +0500

    Пишу поток

--- yandexDisk/yaDiskEasy.py
+++ yandexDisk/yaDiskEasy.py
@@ -10,19 +10,18 @@ class YandexDisk():
             # Безвозвратно удаляет "/file-to-remove"
             y.remove(path, permanently=True)
             sleep(1)
-            y.upload("data.txt", "/temp/data.txt")
+            y.upload("data.txt", path)
         else:
-            y.upload("data.txt", "/temp/data.txt")
+            y.upload("data.txt", path)
 
     def downLoadOneFile(self):
         y.download("/temp/data.txt", "downloaded_data.txt")
-
 
 if __name__ == "__main__":
     app = YandexDisk()
     y = yadisk.YaDisk("f303b8380db54e49b15653a1209dab1f", "7e2a45d0322a4784aca57c998ec24863",
                       "AQAAAAAxhUvqAAV8Wn_FxaChMUjJgVtqGRQx12c")
     app.uploadOneFile()
-    sleep(5)
+    sleep(1)
     #app.downLoadOneFile()
 
